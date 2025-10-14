5ede70dd63b59c5b41af153eb59531006fc4b28f
Sahal-M <sahalsahu0000@gmail.com>
2025-10-14 15:06:18 +0530

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -7,32 +7,32 @@ private:
     int width;
 
 public:
-    // Default constructor
+    
     Rectangle() {
         length = 0;
         width = 0;
     }
 
-    // Parameterized constructor
+   
     Rectangle(int l, int w) {
         length = l;
         width = w;
     }
 
-    // Function to display length and width
+    
     void display() {
         cout << "Length: " << length << ", Width: " << width << endl;
     }
 
-    // Destructor
+    
     ~Rectangle() {
         cout << "Rectangle object destroyed." << endl;
     }
 };
 
 int main() {
-    Rectangle r1;         // default constructor called
-    Rectangle r2(10, 5);  // parameterized constructor called
+    Rectangle r1;         
+    Rectangle r2(10, 5);  
 
     cout << "Rectangle 1: ";
     r1.display();
